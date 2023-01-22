d89413a05ff6d54a60743a05dde7f56ca2934c5e
Rouseta <catacrocken@gmail.com>
2023-01-22 17:00:24 +0100

    try to get calculator no succes

--- appExtra.js
+++ appExtra.js
@@ -3,76 +3,36 @@ const btnRes = document.querySelector('.resta');
 const btnMult = document.querySelector('.mult');
 const btnDiv = document.querySelector('.div');
 const btnEquals = document.querySelector('.equals');
-let numbersInput = () => parseInt(document.querySelector('.screen').value);
+let numbersInput = document.querySelector('.screen').value;
 let inputTotal = document.querySelector('.total');
-
+console.log(numbersInput)
 const operations = {
-    suma: '+',
-    resta: '-',
-    multiplicacion: '*',
-    division: '/',
-    igual: '='
+    sumar: (a, b) => a + b,
+    restar: (a, b) => a - b,
+    multiplicar: (a, b) => a * b,
+    dividir: (a, b) => a / b,
+}
+
+const handleOPeration = (operation) => {
+    return inputTotal.innerHTML = operations[operation]
+
 }
 
 
 
-const sum = (a, b) => {
-    let result = a + b
-    return result
-}
-const handleSum = () => {
-
-    inputTotal.innerHTML = sum(inputA(), inputB())
-}
-
-
-const diff = (a, b) => {
-    let result = a - b
-    return result
-}
-const handleDiff = () => {
-    inputTotal.innerHTML = diff(inputA(), inputB());
-
-}
-
-
-const mult = (a, b) => {
-    let result = a * b
-    return result
-}
-const handleMult = () => inputTotal.innerHTML = mult(inputA(), inputB());
 
 
 
-const div = (a, b) => {
-    const result = a / b
-    return result
-}
 
-const handleDiv = () => inputTotal.innerHTML = div(inputA(), inputB());
-const reset = () => {
-    inputTotal = 0;
-}
-const handleEquals = () => {
-    let res = 0;
-    switch (operations) {
-        case 'suma':
-            res = numbersInput + numbersInput
-            break;
-        case 'resta':
-            res = numbersInput - numbersInput
-            break;
-        case 'multiplicacion':
-            res = numbersInput * numbersInput
-            break;
-        case 'division':
-            res = numbersInput / numbersInput
-            break;
 
-    }
-    console.log(res)
-    inputTotal.innerHTML = res
-}
+
+
+
+
+
+
+
+
 
 
 
